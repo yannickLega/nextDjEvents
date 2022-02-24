41d46506afa9ae8ad7b0ece96223782a63ce92c8
yannickLega <yannick.lega@yahoo.com>
2022-02-24 22:24:30 +0100

    footer correction

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,11 @@ import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
-    <Footer className={styles.footer}>
+    <footer className={styles.footer}>
       <p>Copyright &copy; DJ Events 2022</p>
       <p>
         <Link href="/about">About This Project</Link>
       </p>
-    </Footer>
+    </footer>
   );
 }
