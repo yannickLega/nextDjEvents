9d6394d612ffd12180239a8b0c92798054984eab
yannickLega <yannick.lega@yahoo.com>
2022-03-02 23:01:58 +0100

    add search func and search component

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Search from "@/components/Search";
 import styles from "@/styles/Header.module.css";
 
 export default function Header() {
@@ -9,6 +10,7 @@ export default function Header() {
           <a>DJ Events</a>
         </Link>
       </div>
+      <Search />
       <nav>
         <ul>
           <li>
