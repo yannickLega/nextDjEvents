09d52a1dfa3a84c46a29f203355c434ef8b567b3
yannickLega <yannick.lega@yahoo.com>
2022-03-04 11:33:58 +0100

    add login and register component

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Search from "@/components/Search";
 import styles from "@/styles/Header.module.css";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -21,6 +22,14 @@ export default function Header() {
           <li>
             <Link href="/events/add">
               <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn-secondary btn-icon">
+                <FaSignInAlt />
+                Login
+              </a>
             </Link>
           </li>
         </ul>
